719caaff340bd8271665569941102085b688eceb
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-07-09 23:07:35 +0300

    baseEntity user objects added

--- MediaProductionCompany/MediaProductionCompany.Data/DbEntity/BaseDbEntity.cs
+++ MediaProductionCompany/MediaProductionCompany.Data/DbEntity/BaseDbEntity.cs
@@ -9,10 +9,13 @@ namespace MediaProductionCompany.Data.DbEntity
     public class BaseDbEntity
     {
         public int Id { get; set; }
+        public UserDbEntity InsertUser { get; set; }
         public string InsertUserId { get; set; }
         public DateTime CreatedAt { get; set; }
+        public UserDbEntity UpdateUser { get; set; }
         public string? UpdateUserId  { get; set; }
         public DateTime? UpdatedAt { get; set; }
+        public UserDbEntity DeleteUser { get; set; }
         public string? DeleteUserId { get; set; }
         public DateTime? DeletedAt { get; set; }
         public bool IsDeleted { get; set; }
