8e1f1eb5f4d1bda342c18de040cdc4da5135d1f9
mohammed-abushaban <mohammad.d.h.2000@gmail.com>
2021-07-14 15:58:01 +0300

    auto mapper profile

--- MediaProductionCompany/MediaProductionCompany.Infrastructure/AutoMapper/AutoMapperProfile.cs
+++ MediaProductionCompany/MediaProductionCompany.Infrastructure/AutoMapper/AutoMapperProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using MediaProductionCompany.Core.Dtos;
+using MediaProductionCompany.Core.ViewModels;
+using MediaProductionCompany.Data.DbEntity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +15,28 @@ namespace MediaProductionCompany.Infrastructure.AutoMapper
         public AutoMapperProfile()
         {
             //CreateMap<UpdateProjectDto, ProjectDbEntity>().ForAllMembers(opt => opt.Condition((src, dest, sourcrMember) => sourcrMember != null));
+            CreateMap<CategoryDbEntity, CategoryVM>();
+            CreateMap<CountryDbEntity, CountryVM>();
+            CreateMap<LanguageDbEntity, LanguageVM>();
+            CreateMap<PortfolioDbEntity, PortfolioVM>();
+            CreateMap<PortfolioTranslationDbEntity, PortfolioTranslationVM>();
+            CreateMap<UserDbEntity, UserVM>();
+
+            CreateMap<CreateCategoryDto, CategoryDbEntity>();
+            CreateMap<CreateCountryDto, CountryDbEntity>();
+            CreateMap<CreateLanguageDto, LanguageDbEntity>();
+            CreateMap<CreatePortfolioDto, PortfolioDbEntity>();
+            CreateMap<CreatePortfolioTranslationDto, PortfolioTranslationDbEntity>();
+
+            CreateMap<UpdateCategoryDto, CategoryDbEntity>().ForAllMembers(opt => opt.Condition((src, dest, sourcrMember) => sourcrMember != null));
+            CreateMap<UpdateCountryDto, CountryDbEntity>().ForAllMembers(opt => opt.Condition((src, dest, sourcrMember) => sourcrMember != null));
+            CreateMap<UpdateLanguageDto, LanguageDbEntity>().ForAllMembers(opt => opt.Condition((src, dest, sourcrMember) => sourcrMember != null));
+            CreateMap<UpdatePortfolioDto, PortfolioDbEntity>().ForAllMembers(opt => opt.Condition((src, dest, sourcrMember) => sourcrMember != null));
+            CreateMap<UpdatePortfolioTranslationDto, PortfolioTranslationDbEntity>().ForAllMembers(opt => opt.Condition((src, dest, sourcrMember) => sourcrMember != null));
+
+
+
         }
-       
+
     }
 }
