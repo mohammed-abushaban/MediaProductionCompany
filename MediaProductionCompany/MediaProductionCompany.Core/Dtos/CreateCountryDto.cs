51e7d2543234952711b5a278c0381165c5c452b4
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-07-14 12:26:11 +0300

    All Dtos Done.

---
+++ MediaProductionCompany/MediaProductionCompany.Core/Dtos/CreateCountryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaProductionCompany.Core.Dtos
+{
+    public class CreateCountryDto
+    {
+        public string Name { get; set; }
+        public int LanguageId { get; set; }
+    }
+}
---
+++ MediaProductionCompany/MediaProductionCompany.Core/Dtos/CreatePortoFolioTranslationDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaProductionCompany.Core.Dtos
+{
+    public class CreatePortoFolioTranslationDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IFormFile Attachment { get; set; }
+        public int CategoryId { get; set; }
+        public int LanguageId { get; set; }
+    }
+}
