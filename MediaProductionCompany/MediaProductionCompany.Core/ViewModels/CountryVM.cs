088a22a62962c7665d00304d66f04bfc705a74ef
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-07-14 13:37:30 +0300

    ViewModels Added

---
+++ MediaProductionCompany/MediaProductionCompany.Core/ViewModels/CountryVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaProductionCompany.Core.ViewModels
+{
+    public class CountryVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+    }
+}
---
+++ MediaProductionCompany/MediaProductionCompany.Core/ViewModels/PortfolioTranslationVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaProductionCompany.Core.ViewModels
+{
+    public class PortfolioTranslationVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Attachment { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+    }
+}
