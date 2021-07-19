8914ad188b214a96047d176da19ff8581e698568
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2021-07-19 16:02:54 +0300

    tdod added

--- MediaProductionCompany/MediaProductionCompany.Data/DbEntity/PortfolioDbEntity.cs
+++ MediaProductionCompany/MediaProductionCompany.Data/DbEntity/PortfolioDbEntity.cs
@@ -8,7 +8,10 @@ namespace MediaProductionCompany.Data.DbEntity
 {
     public class PortfolioDbEntity : BaseDbEntity
     {
+
         public string Title { get; set; }
+        //TODO : remove user ID 
+
         public string UserId { get; set; }
         public UserDbEntity User { get; set; }
     }
